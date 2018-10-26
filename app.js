d5a87fd167c81c3902054892b089246b9ec6eb1b
hsloss <hannahmaguiresloss@gmail.com>
2018-10-26 16:16:46 -0400

    solved the stretch goal! problem 6.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
@@ -9,13 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    let sumNumber = a + b
-    let finalstatement = 'The sum of ' + a + ' and ' + b + ' is ' + sumNumber + '.'
-    return [sumNumber, finalstatement]
-  
-} 
+  let sumNumber = a + b
+  let finalstatement = 'The sum of ' + a + ' and ' + b + ' is ' + sumNumber + '.'
+  return [sumNumber, finalstatement]
+
+}
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+testSum(4, 7)
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -35,7 +35,7 @@ function multiply(a, b) { //eslint-disable-line
 }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+testMultiply(5,9)
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -57,14 +57,14 @@ function sumAndMultiply(a,b,c){
   let firstmultiply = multiply(a,b)
   let secondmultiply = multiply(firstmultiply[0], c)
   let thirdelement = a + ' and ' + b + ' and ' + c + ' sum to ' + secondsum[0] + '.'
-  let fourthelement = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + 
+  let fourthelement = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' +
   secondmultiply[0] + '.'
   let testarray = [secondsum[0], secondmultiply[0], thirdelement, fourthelement]
   return testarray
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5)
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -86,14 +86,14 @@ function sumArray(sumArr) { //eslint-disable-line
   console.log(firstsum)
   let secondsum = sum(firstsum[0], sumArr[2])
   console.log(secondsum)
-  let finalstatement = sumArr[0] + ',' + sumArr[1] +',' + sumArr[2] + ' was passed in as an array of numbers, and ' + secondsum[0] + " is their sum."
+  let finalstatement = sumArr[0] + ',' + sumArr[1] +',' + sumArr[2] + ' was passed in as an array of numbers, and ' + secondsum[0] + ' is their sum.'
   console.log(finalstatement)
   return [secondsum[0], finalstatement]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-testSumArray(testArray);
+testSumArray(testArray)
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -145,10 +145,16 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  let dynamicArrayStart = dynamicArray[0]
+  for(let i = 1; i < dynamicArray.length; i++){
+    dynamicArrayStart = dynamicArrayStart * dynamicArray[i]
+  }
+  let finalstatement = 'The numbers ' + dynamicArray + ' have a product of ' + dynamicArrayStart + '.'
+  console.log(finalstatement)
+  return [dynamicArrayStart, finalstatement]
 
 }
-
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray)
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
